3f2aa45b97ba416e359663b27f2a28c5898af65e
Gayan Viduranga <gayanviduranga95@gmail.com>
2026-01-31 21:04:26 +0530

    Cleaned default Vue boilerplate for custom project

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="min-h-screen">
-    <!-- App content will go here -->
+  <div class="min-h-screen bg-pink-50 flex items-center justify-center">
+    <h1 class="text-4xl font-bold text-pink-600">
+      Mother’s Garment Shop 👗
+    </h1>
   </div>
 </template>
 
